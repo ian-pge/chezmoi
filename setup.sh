7a6c7552c556cacf54642aad81a5cde46cf65e82
Ian <82505982+ian-pge@users.noreply.github.com>
2025-05-04 12:53:37 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$HOME/.local/bin" init --apply https://github.com/ian-pge/chezmoi.git
+sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$HOME/.local/bin" chezmoi init --source "$PWD" --apply
