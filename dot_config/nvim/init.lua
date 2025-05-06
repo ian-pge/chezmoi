895c7a7c0566973b9d310cfd6e8a45bc3e647293
Ian <82505982+ian-pge@users.noreply.github.com>
2025-05-06 09:33:52 +0000

    Update init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,6 +1,26 @@
 vim.opt.number = true
 
-require("catppuccin").setup({
-  flavour               = "macchiato",
+-- Bootstrap lazy.nvim if not already installed
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none", "--branch=stable",
+    "https://github.com/folke/lazy.nvim.git", lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)  
+
+-- Load and configure plugins
+require("lazy").setup({
+  {
+    "catppuccin/nvim",
+    name     = "catppuccin",
+    lazy     = false,
+    priority = 1000,
+    config = function()
+      vim.g.catppuccin_flavour = "macchiato"                  
+      require("catppuccin").setup({ flavour = "macchiato" })       
+      vim.cmd.colorscheme("catppuccin-macchiato")                  
+    end,
+  },
 })
-vim.cmd.colorscheme "catppuccin"
